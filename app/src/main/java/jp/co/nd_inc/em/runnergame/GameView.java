444dde129e33d66b31c4d4171654b4dad6cdcbcb
ohtou <fukujinn@gmail.com>
2016-07-15 01:44:04 +0900

    最もシンプルな構成

--- app/src/main/java/jp/co/nd_inc/em/runnergame/GameView.java
+++ app/src/main/java/jp/co/nd_inc/em/runnergame/GameView.java
@@ -2,17 +2,12 @@ package jp.co.nd_inc.em.runnergame;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 
 public class GameView extends SurfaceView
-        implements SurfaceHolder.Callback, Runnable {
+        implements SurfaceHolder.Callback {
     private static final String TAG = "GameView";
 
     // ゲーム領域の幅（ピクセル）
@@ -24,21 +19,11 @@ public class GameView extends SurfaceView
     // 画面サイズに合わせるための縮尺
     private float mScale;
 
-    // フレームの間隔（ミリ秒）
-    private static final long INTERVAL = 20;
-
     private SurfaceHolder mHolder;
-
-    private ScheduledExecutorService mScheduledExecutorService;
-
-    private GameLogic mGameLogic;
-
 
 
     public GameView(Context context) {
         super(context);
-
-        mGameLogic = new GameLogic(context);
 
         mHolder = getHolder();
         mHolder.addCallback(this);
@@ -48,9 +33,22 @@ public class GameView extends SurfaceView
     public void surfaceCreated(SurfaceHolder holder) {
         calcScale();
 
-        // INTERVALの間隔でフレームを描画する。
-        mScheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        mScheduledExecutorService.scheduleAtFixedRate(this, 0, INTERVAL, TimeUnit.MILLISECONDS);
+        Canvas canvas = mHolder.lockCanvas();
+        if (canvas == null) return;
+
+        canvas.scale(mScale, mScale);
+        canvas.clipRect(0, 0, WIDTH, HEIGHT);
+
+        // ===========================
+        // ここに描画処理を書く
+
+
+
+
+
+        // ===========================
+
+        mHolder.unlockCanvasAndPost(canvas);
     }
 
     @Override
@@ -59,24 +57,6 @@ public class GameView extends SurfaceView
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        mScheduledExecutorService.shutdown();
-    }
-
-    @Override
-    public void run() {
-//        Log.i(TAG, "run!");
-        Canvas canvas;
-
-        canvas = mHolder.lockCanvas();
-
-        if (canvas == null) return;
-
-        canvas.scale(mScale, mScale);
-        canvas.clipRect(0, 0, WIDTH, HEIGHT);
-
-        mGameLogic.draw(canvas);
-
-        mHolder.unlockCanvasAndPost(canvas);
     }
 
     private void calcScale() {
@@ -84,24 +64,4 @@ public class GameView extends SurfaceView
         float scaleY = getHeight() /  HEIGHT;
         mScale = (scaleX > scaleY) ? scaleY : scaleX;
     }
-
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int touchAction = event.getAction();
-
-        switch (touchAction) {
-            case MotionEvent.ACTION_DOWN:
-                mGameLogic.onTouchDown();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                break;
-            case MotionEvent.ACTION_UP:
-                mGameLogic.onTouchUp();
-                break;
-        }
-
-        return true;
-    }
-
 }
--- app/src/main/java/jp/co/nd_inc/em/runnergame/activity/MainActivity.java
+++ app/src/main/java/jp/co/nd_inc/em/runnergame/activity/MainActivity.java
@@ -1,18 +1,13 @@
 package jp.co.nd_inc.em.runnergame.activity;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 import jp.co.nd_inc.em.runnergame.GameView;
-import jp.co.nd_inc.em.runnergame.R;
 
 public class MainActivity extends Activity {
-
-    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,32 +18,8 @@ public class MainActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        // BGM用音楽ファイルを読み込み
-        mediaPlayer = MediaPlayer.create(this, R.raw.bgm);
-        mediaPlayer.setLooping(true);
-
         // ゲームViewを設置
         setContentView(new GameView(this));
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        mediaPlayer.start();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        mediaPlayer.stop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        mediaPlayer.release();
-    }
 }

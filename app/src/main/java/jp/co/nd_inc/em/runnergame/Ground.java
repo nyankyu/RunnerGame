623f72d809af3cf3df5daad5a2eec6c06ed41851
ohtou <fukujinn@gmail.com>
2016-11-07 04:43:27 +0900

    ブロック列を右から左へアニメーションさせる。

--- app/src/main/java/jp/co/nd_inc/em/runnergame/Ground.java
+++ app/src/main/java/jp/co/nd_inc/em/runnergame/Ground.java
@@ -9,14 +9,18 @@ import android.graphics.Canvas;
 class Ground {
     private Bitmap block;
     private int[] level = {1,1,1,1,1,1,1,1,1};
+    private float offsetX;
 
     Ground(Context context) {
         block = BitmapFactory.decodeResource(context.getResources(), R.drawable.block);
+        offsetX = 0;
     }
 
     void draw(Canvas canvas) {
+        offsetX += 5;
+
         for (int i = 0; i < level.length; i++) {
-            float x = 100 * i;
+            float x = 100 * i - offsetX;
             float y = GameView.HEIGHT - 100 * level[i];
             canvas.drawBitmap(block, x, y, null);
         }

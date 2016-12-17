ed261d25261b68293bef873e488bbfa7a56651aa
ohtou <fukujinn@gmail.com>
2016-12-17 15:37:47 +0900

    プレイヤーの画像を交互に描画する。

--- app/src/main/java/jp/co/nd_inc/em/runnergame/Player.java
+++ app/src/main/java/jp/co/nd_inc/em/runnergame/Player.java
@@ -12,16 +12,33 @@ public class Player {
 
     private int positionY;
 
+    private int drawCounter;
+    private boolean drawBitmap1;
 
 
     Player(Context context) {
         bitmap1 = BitmapFactory.decodeResource(context.getResources(), R.drawable.player1);
-        bitmap1 = BitmapFactory.decodeResource(context.getResources(), R.drawable.player2);
+        bitmap2 = BitmapFactory.decodeResource(context.getResources(), R.drawable.player2);
 
         positionY = 250;
+
+        drawCounter = 0;
+        drawBitmap1 = true;
     }
 
     void draw(Canvas canvas) {
-        canvas.drawBitmap(bitmap1, 0, positionY, null);
+        drawCounter++;
+
+        if (drawCounter > 10) {
+            // フラグを反転
+            drawBitmap1 ^= true;
+            drawCounter = 0;
+        }
+
+        if (drawBitmap1) {
+            canvas.drawBitmap(bitmap1, 0, positionY, null);
+        } else {
+            canvas.drawBitmap(bitmap2, 0, positionY, null);
+        }
     }
 }

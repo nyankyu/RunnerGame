fc6c2fafb07f5100f65f0df8d5465afcc3f9e3d3
ohtou <fukujinn@gmail.com>
2017-01-21 12:30:01 +0900

    BGMを追加

--- app/src/main/java/jp/co/nd_inc/em/runnergame/MainActivity.java
+++ app/src/main/java/jp/co/nd_inc/em/runnergame/MainActivity.java
@@ -1,12 +1,15 @@
 package jp.co.nd_inc.em.runnergame;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 
 public class MainActivity extends Activity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,28 @@ public class MainActivity extends Activity {
 
         // ゲームViewを設置
         setContentView(new GameView(this));
+
+        // BGM用音楽ファイルを読み込み
+        mediaPlayer = MediaPlayer.create(this, R.raw.bgm);
+        mediaPlayer.setLooping(true);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
 }

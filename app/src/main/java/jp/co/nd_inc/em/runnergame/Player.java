6e18fba03d0b437d2d5a377a99286ef5163629f5
ohtou <fukujinn@gmail.com>
2017-02-15 01:25:56 +0900

    SoundPoolの生成方法をビルドバージョンで場合分け

--- app/src/main/java/jp/co/nd_inc/em/runnergame/Player.java
+++ app/src/main/java/jp/co/nd_inc/em/runnergame/Player.java
@@ -1,10 +1,13 @@
 package jp.co.nd_inc.em.runnergame;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Build;
 import android.util.Log;
 
 
@@ -43,8 +46,14 @@ public class Player {
 
         gameover = false;
 
-        soundPool = new SoundPool.Builder().setMaxStreams(1).build();
-        soundPool.load(context, R.raw.se_jump, 1);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        } else {
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(1)
+                    .build();
+        }
+        jumpSound = soundPool.load(context, R.raw.se_jump, 1);
     }
 
     void draw(Canvas canvas) {
